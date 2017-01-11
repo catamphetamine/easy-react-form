da3b384e45a5ad823719e04f07606fdc9f22ddf7
Nikolay <halt.hammerzeit.at@gmail.com>
2017-01-11 21:22:06 +0300

    Added ES6 exports

--- index.es6.js
+++ index.es6.js
@@ -1,4 +1,4 @@
-export { default as default } from './source/form'
-export { default as Field }   from './source/field'
-export { default as Submit }  from './source/submit'
-export { default as reducer } from './source/reducer'
+export { default as default } from './es6/form'
+export { default as Field }   from './es6/field'
+export { default as Submit }  from './es6/submit'
+export { default as reducer } from './es6/reducer'
